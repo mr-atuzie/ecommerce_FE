8ae3c791c07f461d3af93d58635797888ff723ae
mr-atuzie <rexatuzie@gmail.com>
2024-01-07 13:26:35 +0100

    authService setup/.env

---
+++ src/redux/features/auth/authService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const API_URL = `${BACKEND_URL}/api/v1/users/`;
+
+//Register User
+const registerUser = async (userData) => {
+  try {
+    const res = await axios.post(API_URL + "register", userData);
+  } catch (error) {}
+};
