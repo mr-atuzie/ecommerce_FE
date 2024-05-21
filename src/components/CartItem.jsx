0b2bd98f1c1f502270525b368f226c58de4f6f1e
mr-atuzie <user email>
2024-05-22 00:54:24 +0100

    cart item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -6,38 +6,52 @@ const CartItem = ({ id, imageurl, name, price }) => {
   return (
     <Link
       to={`/product/${id}`}
-      className=" flex gap-2 bg-white mb-4 border shadow-md overflow-hidden"
+      className=" flex items-center gap-2 bg-white mb-4 "
     >
-      <div className=" w-24">
-        {imageurl && (
-          <img
-            className="  w-full object-cover aspect-square "
-            src={imageurl}
-            alt=""
-          />
-        )}
-      </div>
-      <div className=" w-full p-2">
-        <h2 className=" font-medium leading-5 text-sm">{name}</h2>
+      {imageurl && (
+        <img
+          className="rounded-xl  w-20 object-cover aspect-square "
+          src={imageurl}
+          alt=""
+        />
+      )}
+
+      <div className=" w-full px-2">
+        <h2 className=" font-semibold leading-5 text-sm truncate">{name}</h2>
         <div className="flex items-center mt-0.5 justify-between">
           <p className="font-bold">${USDollar.format(price)}</p>
 
-          <button className=" rounded-md  bg-emerald-500 text-white font-medium text-sm   flex justify-center items-center p-1">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-4 h-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-              />
-            </svg>
-          </button>
+          <div className=" flex items-center gap-3">
+            <button className=" rounded-md  bg-emerald-500 text-white font-medium text-sm   flex justify-center items-center p-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+            <span className=" text-sm font-medium">1</span>
+            <button className=" rounded-md  text-emerald-500 border border-emerald-500 font-medium text-sm   flex justify-center items-center p-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-4 h-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
     </Link>
